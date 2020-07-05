6c3af319a2bc4df47cf0a5d9ba42f3d1d2470cd8
carpentryWorld <67862954+carpentryWorld@users.noreply.github.com>
2020-07-05 15:21:40 +0300

    carpentryWorld basic portfolio website

---
+++ precache-manifest.94a28f1d254ba8eb87cd3a5985a86ee2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ce4e77d826eb674823dc7eb7c044b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f30eee87a0c1122ed2c",
+    "url": "/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "4fce34820b76f83c081a",
+    "url": "/static/css/main.b1fff806.chunk.css"
+  },
+  {
+    "revision": "3f30eee87a0c1122ed2c",
+    "url": "/static/js/2.b41c3106.chunk.js"
+  },
+  {
+    "revision": "ff6876a3098212834b5f0844f0d5786d",
+    "url": "/static/js/2.b41c3106.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fce34820b76f83c081a",
+    "url": "/static/js/main.a85d293c.chunk.js"
+  },
+  {
+    "revision": "2b0dad28a6d1c6301fef",
+    "url": "/static/js/runtime-main.1b8aa31f.js"
+  }
+]);
